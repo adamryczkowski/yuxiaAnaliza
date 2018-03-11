8b510df779f45a23487fde3bfee980823f3d62b1
Adam Ryczkowski <adam@statystyka.net>
2018-03-11 22:16:01 +0100

    changes

--- analiza.R
+++ analiza.R
@@ -6,7 +6,7 @@ macierze_path<-system.file('macierze_analiz.xlsx', package='yuxiaAnaliza')
 m<-relationshipMatrix::read_matrix(macierze_path, data.table(dt_structure), aggregate_types = aggrt)
 cl<-yuxiaCharts::classify_analyses(m)
 tododf<-cl$tododf
-subset_df<-filter(tododf, dispatcher %in% c('boxplot_wyliczany', 'crosstab', 'boxplot'))
+subset_df<-data.table(dplyr::filter(tododf, dispatcher %in% c('boxplot_wyliczany', 'crosstab', 'boxplot')))
 doc<-relationshipMatrix::render_matrix(cellsdf=subset_df, author="Adam", title="analiza",
                                        stats_dispatchers=cl$dispatchers,
                                        report_dispatchers=list(),
