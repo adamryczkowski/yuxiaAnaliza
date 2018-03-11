dc379679385c6b1402946587005d916c0e536101
Adam Ryczkowski <adam@statystyka.net>
2018-03-11 21:17:57 +0100

    x

--- analiza.R
+++ analiza.R
@@ -1,6 +1,14 @@
+library(data.table)
 dt<-readRDS('db.rds')
 dt_structure<-danesurowe::create_df_from_df_structure(dt, flag_include_vartype = TRUE)
 aggrt<-allAggregates()
 macierze_path<-system.file('macierze_analiz.xlsx', package='yuxiaAnaliza')
 m<-relationshipMatrix::read_matrix(macierze_path, data.table(dt_structure), aggregate_types = aggrt)
-library(data.table)
+cl<-yuxiaCharts::classify_analyses(m)
+tododf<-cl$tododf
+subset_df<-filter(tododf, dispatcher %in% c('boxplot_wyliczany', 'crosstab', 'boxplot'))
+doc<-relationshipMatrix::render_matrix(cellsdf=subset_df, author="Adam", title="analiza",
+                                       stats_dispatchers=cl$dispatchers,
+                                       report_dispatchers=list(),
+                                       report_functions=list(),
+                                       aggregates=aggrt, filters=get_filters(), df_task=dt)
