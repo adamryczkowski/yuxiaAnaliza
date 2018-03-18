acdf549d22ffd4521f5f9b528c1c6dae553e7f19
adam <aryczkowski@imgw.pl>
2018-03-18 12:59:47 +0100

    popr macierze analiz

--- analiza.R
+++ analiza.R
@@ -7,22 +7,38 @@ macierze_path<-system.file('macierze_analiz.xlsx', package='yuxiaAnaliza')
 m<-relationshipMatrix::read_matrix(macierze_path, data.table(dt_structure), aggregate_types = aggrt)
 cl<-yuxiaCharts::classify_analyses(m)
 tododf<-cl$tododf
-subset_df<-data.table(dplyr::filter(tododf, dispatcher %in% c('boxplot_wyliczany', 'crosstab', 'boxplot')))
+subset_df<-data.table(dplyr::filter(tododf, dispatcher %in% c('boxplot_wyliczany', 'crosstab', 'boxplots', 'ts_nominal', 'ts_trend')))
 subset_df<-data.table(dplyr::filter(tododf, dispatcher %in% c('boxplot')))
 subset_df<-data.table(dplyr::filter(tododf, dispatcher %in% c('boxplot_wyliczany')))
 subset_df<-data.table(dplyr::filter(tododf, dispatcher %in% c('crosstab')))
 subset_df<-data.table(dplyr::filter(tododf, dispatcher %in% c('ts_nominal')))
+subset_df<-data.table(dplyr::filter(tododf, dispatcher %in% c('ts_trend')))
 
-<<<<<<< HEAD
-doc<-relationshipMatrix::render_matrix(cellsdf=subset_df, author="Adam", title="Nominal trend",
-=======
+dvs<-unique(unlist(subset_df$prefix2))
+doc_dir<-'/home/Adama-docs/Adam/MyDocs/Statystyka/Aktywne analizy/Yu Xia/yuxia-local/raportyAR/raporty/Podstawowe'
+chart_dir<-'/home/Adama-docs/Adam/MyDocs/Statystyka/Aktywne analizy/Yu Xia/yuxia-local/raportyAR/wykresy/Podstawowe'
+dv<-dvs[[1]]
+for(dv in dvs) {
+  dv_df<-data.table(subset_df[purrr::map_lgl(subset_df$prefix2, ~ dv %in% unlist(.)),])
+  doc<-relationshipMatrix::render_matrix(cellsdf=dv_df[seq(2, nrow(dv_df)),], author="Adam", title=dv,
+                                         stats_dispatchers=cl$dispatchers,
+                                         report_dispatchers=list(),
+                                         chart_foldername=chart_dir,
+                                         report_functions=list(), header_depth_offset=4, flag_add_chapter_for_each_cell = FALSE,
+                                         aggregates=aggrt, filters=yuxiaAnaliza::get_filters(), df_task=dt)
+  doc$set_property('chart_debug', TRUE)
+  doc$pre_render()
+  saveRDS(doc, file=pathcat::path.cat(doc_dir, paste0('ch_', which(dv %in% dvs), '.rds')), compress='xz')
+  doc$render(pandoc)
+  save_report(pandoc,  filename = pathcat::path.cat(doc_dir, paste0('ch_', which(dv %in% dvs))))
+}
+
 doc<-relationshipMatrix::render_matrix(cellsdf=subset_df, author="Adam", title="Time series vs nominal",
->>>>>>> 01f27a8e9687268c79e9053b6a25292a7aa1d090
                                        stats_dispatchers=cl$dispatchers,
                                        report_dispatchers=list(),
                                        report_functions=list(), header_depth_offset=4, flag_add_chapter_for_each_cell = FALSE,
                                        aggregates=aggrt, filters=yuxiaAnaliza::get_filters(), df_task=dt)
-doc<-relationshipMatrix::render_matrix(cellsdf=subset_df[c(46),], author="Adam", title="analiza",
+doc<-relationshipMatrix::render_matrix(cellsdf=subset_df[c(1),], author="Adam", title="analiza",
                                        stats_dispatchers=cl$dispatchers,
                                        report_dispatchers=list(),
                                        report_functions=list(), flag_add_chapter_for_each_cell = FALSE,
@@ -35,11 +51,7 @@ doc<-relationshipMatrix::render_matrix(cellsdf=subset_df[c(46, 47, 49,  96, 97,9
 doc$set_property('chart_debug', TRUE)
 doc$set_property('chart_postprocess', FALSE)
 doc$pre_render()
-<<<<<<< HEAD
 saveRDS(doc, file='doc_crosstab.rds', compress='xz')
-=======
-saveRDS(doc, file='doc_boxploty.rds', compress='xz')
->>>>>>> 01f27a8e9687268c79e9053b6a25292a7aa1d090
 pandoc<-pander::Pandoc$new()
 doc<-readRDS('doc2.rds')
 rm(dt)
